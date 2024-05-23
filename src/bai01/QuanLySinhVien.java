12930838061f28a05ed32cfa29d9c0f5e568a7dc
Admin <Admin@DESKTOP-887CCMB>
2024-05-23 20:00:15 +0700

    thu 4 ngay 22 thang 5

---
+++ src/bai01/QuanLySinhVien.java
@@ -0,0 +1,13 @@
+package bai01;
+
+
+public class QuanLySinhVien {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		SinhVien[] sv;
+		GiangVien gvA, gvB;
+	}
+
+}
