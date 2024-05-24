f449b4130af18d2313576bc209c2181914960729
Admin <Admin@DESKTOP-887CCMB>
2024-05-24 09:34:58 +0700

    bai03-vd3

---
+++ src/bai03/vd3/QuanLySinhVien.java
@@ -0,0 +1,15 @@
+package bai03.vd3;
+
+public class QuanLySinhVien {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		SinhVien sv = new SinhVien();
+		sv.setHoten("Doa");
+		sv.setNamsinh(2005);
+		sv.setSdt("9485934");
+		System.out.print(sv);
+	}
+
+}
