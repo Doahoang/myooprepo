05d23513d81764f51937c948729756923e492882
Admin <Admin@DESKTOP-887CCMB>
2024-05-24 09:04:48 +0700

    bai03.vd2

--- src/bai03/vd2/QuanLySinhVien.java
+++ src/bai03/vd2/QuanLySinhVien.java
@@ -4,7 +4,11 @@ public class QuanLySinhVien {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		SinhVien sv = new SinhVien();
+		sv.hoten = "Doa";
+		sv.namsinh = 2005;
+		sv.sdt = "0399033926";
+		System.out.println(sv);
 	}
 
 }
