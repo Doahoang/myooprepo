b49cd43c2005ec8bbca3b5350aaf0c5b701dc7e2
Admin <Admin@DESKTOP-887CCMB>
2024-05-25 16:04:30 +0700

    bai03m - vd4

---
+++ src/bai03/vd4/QuanLySinhVien.java
@@ -0,0 +1,15 @@
+package bai03.vd4;
+
+public class QuanLySinhVien {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		SinhVien sv = new  SinhVien("Doa", 2005 , "0399033926");
+		System.out.println("Ten: "+sv.getHoten());
+		System.out.println("Nam: "+sv.getNamsinh());
+		System.out.println("SDT: "+sv.getSdt());
+		
+	}
+
+}
