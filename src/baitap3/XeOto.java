1cbb45f7def498dbbd9212e7fa2390aa3fb202be
Admin <Admin@DESKTOP-887CCMB>
2024-05-26 23:37:29 +0700

    bai tap ve nha so 3

---
+++ src/baitap3/XeOto.java
@@ -0,0 +1,31 @@
+package baitap3;
+
+public class XeOto {
+	private String hangxe;
+	private int namsanxuat;
+	private float dongco;
+	public String getHangxe() {
+		return hangxe;
+	}
+	public void setHangxe(String hangxe) {
+			this.hangxe = hangxe;
+	}
+	public int getNamsanxuat() {
+		return namsanxuat;
+	}
+	public void setNamsanxuat(int namsanxuat) {
+		this.namsanxuat = namsanxuat;
+	}
+	public float getDongco() {
+		return dongco;
+	}
+	public void setDongco(float dongco) {
+		this.dongco = dongco;
+	}
+	@Override
+	public String toString() {
+		return "XeOto [hangxe=" + hangxe + ", namsanxuat=" + namsanxuat + ", dongco=" + dongco + "]";
+	}
+	
+}
+
